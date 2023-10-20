621c950e6c2584111a0a414a68cca63d896134d7
Eveghu <eveguther546@gmail.com>
2023-10-20 09:21:04 -0600

    Correcciones

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 @section('title','Trainers')
 @section ('content')
@@ -9,10 +8,9 @@
 @foreach ($trainer as $trainer)
 <div class="col-sm">
     <div class="card text-center" style="width: 18rem; margin-top: 70px;">
-    <img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;" 
-    class="card-img-top rounded-circle mx-auto d-block"
-    src="images/{{$trainer->avatar}}" alt="">
-    <div class="card-body">
+      <img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;"
+      class="card-img-top rounded-circle mx-auto d-block"
+      src="avatars/{{$trainer->avatar}}" alt="">
       <h5 class="card-title">{{$trainer ->name}}</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="/delete/{{$trainer->id}}" class="btn btn-primary">DELETE</a>
@@ -23,4 +21,3 @@
 @endforeach
 </div>
 @endsection
-
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,7 +4,7 @@
 <div class="card text-center" style="width: 18rem; margin-top: 70px;">
     <img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;" 
     class="card-img-top rounded-circle mx-auto d-block"
-    src="images/{{$trainer->avatar}}" alt="">
+    src="avatars/{{$trainer->avatar}}" alt="">
     <div class="card-body">
       <h5 class="card-title">{{$trainer ->name}}</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
